0ec0fb9b236a92322b167f401fec2e60c588a8d5
Minerva_juppiter <94231606+minerva-jupiter@users.noreply.github.com>
2022-07-14 23:26:29 +0900

    プロジェクト ファイルを追加します。

---
+++ AttendanceSupport/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AttendanceSupport.Services;
+using AttendanceSupport.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AttendanceSupport
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AttendanceSupport/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AttendanceSupport.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AttendanceSupport.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
